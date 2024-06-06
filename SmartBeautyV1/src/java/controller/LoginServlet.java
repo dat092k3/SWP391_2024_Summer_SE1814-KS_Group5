c4c358e4f91edc1757369d80cb6fe357bb348b1a
phamducdat13 <datpdhs176052@fpt.edu.vn>
2024-06-06 08:16:10 +0700

    hold data login faile

--- SmartBeautyV1/src/java/controller/LoginServlet.java
+++ SmartBeautyV1/src/java/controller/LoginServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import DAO.AccountDAO;
@@ -18,37 +17,41 @@ import model.Account;
 
 /**
  * login to system
+ *
  * @author LENOVO
  */
 public class LoginServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");  
+            out.println("<title>Servlet LoginServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,12 +59,13 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -69,9 +73,9 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         HttpSession session = request.getSession();
-        AccountDAO d= new AccountDAO();
+        AccountDAO d = new AccountDAO();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String rememmber = request.getParameter("remember");
@@ -80,6 +84,10 @@ public class LoginServlet extends HttpServlet {
         Cookie cpassword = new Cookie("cpassword", password);
         if (account == null) {
             session.setAttribute("error_login", "your information is incorrect!");
+            cusername.setMaxAge(60); // Keep username for a short time
+            cpassword.setMaxAge(60); // Keep password for a short time
+            response.addCookie(cusername);
+            response.addCookie(cpassword);
             response.sendRedirect("signup-signin.jsp");
         } else {
             session.removeAttribute("error_login");
@@ -87,21 +95,23 @@ public class LoginServlet extends HttpServlet {
             session.setAttribute("role", account.getRole());
             session.setMaxInactiveInterval(60 * 60 * 3);
             if (rememmber != null && rememmber.equalsIgnoreCase("1")) {
-                cusername.setMaxAge(60);
-                cpassword.setMaxAge(60);
+                cusername.setMaxAge(60 * 60);
+                cpassword.setMaxAge(60 * 60);
                 response.addCookie(cpassword);
             } else {
                 cusername.setMaxAge(0);
                 cpassword.setMaxAge(0);
                 response.addCookie(cpassword);
             }
+            response.addCookie(cpassword);
             response.addCookie(cusername);
             response.sendRedirect("index.jsp");
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
