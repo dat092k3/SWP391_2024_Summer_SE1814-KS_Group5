d3f71766507b86de920f74586852ac4065401a5d
thayvinh <TruongNVHE170937@fpt.edu.vn>
2024-06-08 07:48:10 +0700

    DB not connection

--- SmartBeautyV1/src/java/context/DBContext.java
+++ SmartBeautyV1/src/java/context/DBContext.java
@@ -1,3 +1,4 @@
+
 package context;
 
 import java.sql.Connection;
@@ -5,13 +6,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBContext {
-
+    
     protected Connection connection;
-
-    public DBContext() {
+    public DBContext()
+    {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP391_Version1";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP391_Version1";
             String username = "sa";
             String password = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
