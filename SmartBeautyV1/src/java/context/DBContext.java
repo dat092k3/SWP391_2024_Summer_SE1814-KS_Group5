d4407f50bfdccb0104b1acb493ef3f3102eecdce
thayvinh <TruongNVHE170937@fpt.edu.vn>
2024-06-17 09:47:39 +0700

    jfd

--- SmartBeautyV1/src/java/context/DBContext.java
+++ SmartBeautyV1/src/java/context/DBContext.java
@@ -5,8 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DBContext {
-    
     protected Connection connection;
     public DBContext()
     {
